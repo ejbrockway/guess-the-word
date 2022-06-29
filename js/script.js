55ff036efb35239901d2f586b4d9646f17033de5
emily <ejbrockway@gmail.com>
2022-06-29 17:32:50 -0400

    validation challenege2

--- js/script.js
+++ js/script.js
@@ -9,6 +9,7 @@ const message = document.querySelector(".message");
 const playAgainButton = document.querySelector(".play-again");
 
 const word = "magnolia";
+const guessedLetters = [];
 
 //Display symmbols as place holders for letters//
 const placeHolder = function (word) {
@@ -25,7 +26,44 @@ placeHolder(word);
 
 textButton.addEventListener("click", function (e) {
     e.preventdefault();
+    message.innerText = "";
+
     const guess = letterInput.value;
-    console.log(guess);
+    const goodGuess = validateInput(guess);
+    
+    if (goodGuess){
+        makeGuess(guess);
+    };
+
     letterInput.value = "";
 });
+
+const validateInput = function(input) {
+    const acceptedLetter = /[a-zA-Z]/;
+
+    if (input.length === 0) {
+        message.innerText = "Please enter letter.";
+
+    } else if (input.length > 1) {
+        message.innerText = "Only use one letter at a time.";
+
+     } else if (!input.match(acceptedLetter)) {
+        message.innerText = "Pick a letter from A to Z!";
+
+     } else {
+        return input;
+     }
+
+};
+
+const makeGuess = function (guess) {
+    guess = toUpperCase();
+
+    if (guessedLetters.includes(guess)) {
+        message.innerText = "Try again, we've seen this letter before.";
+
+    } else {
+        guessedLetters.push(guess);
+        console.log (guessedLetters);
+    }
+};
